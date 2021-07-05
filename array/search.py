1570948912d2ecab327f25c4707a168d5ac3ec74
Binh Le <levanbinh1605@gmail.com>
2021-07-05 15:31:25 +0700

    Implement search algos

--- array/search.py
+++ array/search.py
@@ -33,12 +33,43 @@ class Solution(object):
             possible_set.add(sum - num)
         return False
 
+    def valid_mountain_array(self, arr):
+        length = len(arr)
+        if length < 3: return False
+        if length == 3:
+            if arr[1] > arr[0] and arr[1] > arr[2]: return True
+            else: return False
+
+        top = False
+        for i in range(1, length - 1):
+            cur = arr[i]
+            nxt = arr[i + 1]
+            if not top:
+                prev = arr[i - 1]
+                if cur <= prev: return False
+                if cur > nxt: top = True
+                if cur == nxt: return False
+            else:
+                if cur <= nxt: return False
+
+        return top
+
+    def valid_mountain_array_v2(self, arr):
+        top = 0
+        length = len(arr)
+        # walk up to find the top, the order must be strictly increasing
+        while top < length - 1 and arr[top] < arr[top + 1]:
+            top += 1
+
+        # the top must not be start or end of array
+        if top == 0 or top == length - 1: return False
+
+        # walk down from the top, the order must be strictly increasing
+        while top < length - 1 and arr[top] > arr[top + 1]:
+            top += 1
+        return top == length - 1
+
 solution = Solution()
-arr1 = [10,2,5,3]
-arr2 = [7,1,14,11]
-arr3 = [10,2,20,3]
-arr4 = [-2,0,10,-19,4,6,-8]
-arr5 = [0, 0]
-# exist = solution.check_if_exist(arr4)
-exist = solution.check_if_exist_pair_has_num(arr1, 15)
+arr1 = [2, 0, 2]
+exist = solution.valid_mountain_array_v2(arr1)
 print(exist)
